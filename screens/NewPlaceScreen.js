fe81f9e35c3f2a16b493bb72428cc4443f94ccb3
Andrea Rosales <andrearosr@gmail.com>
2021-07-27 19:54:35 -0500

    Create new place flow

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,17 +1,53 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, Button, TextInput, ScrollView, StyleSheet } from 'react-native'
+import { useDispatch } from 'react-redux';
+import { COLORS } from '../constants'
+import { addPlace } from '../store/places.action';
 
-const NewPlaceScreen = () => {
+const NewPlaceScreen = ({ navigation }) => {
+    const dispatch = useDispatch();
+    const [title, setTitle] = useState('');
+
+    const onHandlerTitle = text => setTitle(text);
+    const onHandlerSave = () => {
+        dispatch(addPlace(title));
+        navigation.goBack();
+    }
+
     return (
-        <View style={styles.container}>
-            <Text>Nueva Direccion</Text>
-        </View>
+        <ScrollView>
+            <View style={styles.container}>
+                <Text style={styles.label}>Titulo</Text>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={onHandlerTitle}
+                    value={title}
+                />
+                <Button
+                    title="Grabar Dirección"
+                    color={COLORS.MAROON}
+                    onPress={onHandlerSave}
+                />
+            </View>
+        </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        margin: 30,
+    },
+    label: {
+        fontSize: 18,
+        marginBottom: 16,
+    },
+    input: {
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 1,
+        marginBottom: 16,
+        paddingHorizontal: 2,
+        paddingVertical: 4,
     }
 })
 
