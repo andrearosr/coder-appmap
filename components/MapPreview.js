d579264096d2da97d67fa03da518dd4da2804a14
Andrea Rosales <andrearosr@gmail.com>
2021-08-03 21:01:22 -0500

    Fix url trailing spaces

--- components/MapPreview.js
+++ components/MapPreview.js
@@ -4,15 +4,13 @@ import { MAP } from '../constants';
 
 const MapPreview = ({ location, style, children }) => {
   const loc = location || {}
-  const mapPreviewUrl = `
-    https://maps.googleapis.com/maps/api/staticmap?
+  const mapPreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?
       center=${loc.lat},${loc.lng}
       &zoom=13
       &size=600x300
       &maptype=roadmap
       &markers=color:blue%7Clabel:S%7C${loc.lat},${loc.lng}
-      &key=${MAP.API_KEY}
-  `;
+      &key=${MAP.API_KEY}`;
 
   return (
     <View style={{ ...styles.mapPreview, ...style }}>
